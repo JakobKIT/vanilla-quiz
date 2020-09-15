65e5b21944bc39489f82425230cb427c989161ff
Jakob Klamser <mail@jakobklamser.net>
2020-09-15 18:21:28 +0200

    added a testing route to check if the questions get returned

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const testURL = 'https://opentdb.com/api.php?amount=1&category=15&difficulty=medium&type=multiple';
+
+
+
+const getQuestion = async () => {
+  try {
+    let response = await fetch(testURL);
+    console.log(await response.json());
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getQuestion();
+
 console.log('Hi it works');
