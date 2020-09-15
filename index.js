5a243950b3ce09bb41081be6b23a446e89df6147
Jakob Klamser <mail@jakobklamser.net>
2020-09-15 18:31:59 +0200

    displayed question for testing purpose

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const testURL = 'https://opentdb.com/api.php?amount=1&category=15&difficulty=medium&type=multiple';
 
-
+const quiz = document.querySelector('#question');
 
 const getQuestion = async () => {
   try {
     let response = await fetch(testURL);
-    console.log(await response.json());
+    let result = await response.json();
+    return result;
   } catch (error) {
     console.log(error);
   }
 };
 
-getQuestion();
+getQuestion().then(result => {
+  console.log(result);
+  quiz.innerHTML = result.results[0].question});
 
 console.log('Hi it works');
