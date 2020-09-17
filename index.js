6db6954efd7cdbf63f2a079877201dd19d9fb019
Jakob Klamser <mail@jakobklamser.net>
2020-09-17 15:36:41 +0200

    More styling and questions added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const testURL = 'https://opentdb.com/api.php?amount=1&category=15&difficulty=medium&type=multiple';
 
-const quiz = document.querySelector('#question');
+const question = document.querySelector('#question');
+const a1 = document.querySelector('#a1');
+const a2 = document.querySelector('#a2');
+const a3 = document.querySelector('#a3');
+const a4 = document.querySelector('#a4');
 
 const getQuestion = async () => {
   try {
@@ -14,6 +18,21 @@ const getQuestion = async () => {
 
 getQuestion().then(result => {
   console.log(result);
-  quiz.innerHTML = result.results[0].question});
+  const quiz = result.results[0];
+  let answers = [quiz.correct_answer, ...quiz.incorrect_answers];
+  // Shuffle the array (Fisher-Yates Algorithm)
+  for (let i = answers.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * i)
+    const temp = answers[i]
+    answers[i] = answers[j]
+    answers[j] = temp
+  }
+
+  a1.innerHTML += answers[0];
+  a2.innerHTML += answers[1];
+  a3.innerHTML += answers[2];
+  a4.innerHTML += answers[3];
+  
+  question.innerHTML = quiz.question });
 
 console.log('Hi it works');
