d2ae9ab0d70779c350961bd96743c934fac35c7c
Jakob Klamser <mail@jakobklamser.net>
2020-09-20 13:09:14 +0200

    removed unused code

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
 import Settings from './quiz/settings.js';
 
 new Settings();
-
-
-/* 
-const getQuestion = async () => {
-  try {
-    let response = await fetch(testURL);
-    let result = await response.json();
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-getQuestion().then(result => {
-  console.log(result);
-  const quiz = result.results[0];
-  let answers = [quiz.correct_answer, ...quiz.incorrect_answers];
-
-  // Shuffle the array (Fisher-Yates Algorithm)
-  for (let i = answers.length - 1; i > 0; i--){
-    const j = Math.floor(Math.random() * i)
-    const temp = answers[i]
-    answers[i] = answers[j]
-    answers[j] = temp
-  }
-
-  a1.innerHTML += answers[0];
-  a2.innerHTML += answers[1];
-  a3.innerHTML += answers[2];
-  a4.innerHTML += answers[3];
-
-  question.innerHTML = quiz.question 
-}); */
